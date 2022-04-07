dca09d031696bdd65c6dd2ed822b3c9a6132e714
RogersCarvalho <rogerssud@yahoo.com.br>
2022-04-07 10:39:10 -0300

    dsLearn Offer

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Course.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Course.java
@@ -23,12 +23,12 @@ public class Course implements Serializable {
 	private String imgUri;
 	private String imgGrayUri;
 	
-	//@OneToMany(mappedBy = "course")
-	//private List<Offer> offers = new ArrayList<>();
 	
-	public Course() {
-	}
-
+	@OneToMany(mappedBy = "course")
+	private List<Offer> offers = new ArrayList<>();
+	
+	
+	public Course() {}
 	public Course(Long id, String name, String imgUri, String imgGrayUri) {
 		super();
 		this.id = id;
@@ -36,44 +36,26 @@ public class Course implements Serializable {
 		this.imgUri = imgUri;
 		this.imgGrayUri = imgGrayUri;
 	}
+    
+	public List<Offer> getOffers() {return offers;}
+	public Long getId() {return id;}
+    public void setId(Long id) {this.id = id;}
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+    public String getImgUri() {return imgUri;}
+    public void setImgUri(String imgUri) {this.imgUri = imgUri;}
+    public String getImgGrayUri() {return imgGrayUri;}
+    public void setImgGrayUri(String imgGrayUri) {this.imgGrayUri = imgGrayUri;}
 
-	public Long getId() {
-		return id;
-	}
+	
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getImgUri() {
-		return imgUri;
-	}
-
-	public void setImgUri(String imgUri) {
-		this.imgUri = imgUri;
-	}
-
-	public String getImgGrayUri() {
-		return imgGrayUri;
-	}
-
-	public void setImgGrayUri(String imgGrayUri) {
-		this.imgGrayUri = imgGrayUri;
-	}
-
-	//public List<Offer> getOffers() {
-	//	return offers;
-	//}
-
-	@Override
+    
+    
+    
+    
+    
+    
+    @Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
@@ -97,4 +79,7 @@ public class Course implements Serializable {
 			return false;
 		return true;
 	}
+	
+
+
 }
